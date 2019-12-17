3ddb22e6ed2dbf81766ca5360ef60a6dde4a1daa
AkeKung <kingake555@gmail.com>
2019-12-17 16:36:12 +0700

    first

---
+++ js/main.js
@@ -0,0 +1,11 @@
+console.log('KranLnwza555')
+    
+    let str='';
+    for (let i=0; i<10;i++){
+        console.log(i+1);
+        str+=i+1;
+    }
+
+    console.log(str);
+
+    document.querySelector('#JOJO').innerHTML=str;
